556756cc04bff6ceac7eb9ce78290f6947a136aa
Elena Cristescu <elenacristescu11@gmail.com>
2024-03-03 15:46:17 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialMembers = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    bring: "pizza",
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    bring: "pasta",
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    bring: "",
+  },
+];
+
+const foodItems = [
+  {
+    id: "pizza",
+    name: "pizza",
+    image: "https://foodish-api.com/images/pizza/pizza2.jpg",
+    selected: false,
+  },
+  {
+    id: "pasta",
+    name: "pasta",
+    image: "https://foodish-api.com/images/pasta/pasta2.jpg",
+    selected: false,
+  },
+  {
+    id: "burger",
+    name: "burger",
+    image: "https://foodish-api.com/images/burger/burger5.jpg",
+    selected: false,
+  },
+  {
+    id: "desert",
+    name: "desert",
+    image: "https://foodish-api.com/images/dessert/dessert2.jpg",
+    selected: false,
+  },
+  {
+    id: "rice",
+    name: "rice",
+    image: "https://foodish-api.com/images/rice/rice8.jpg",
+    selected: false,
+  },
+  {
+    id: "samosa",
+    name: "samosa",
+    image: "https://foodish-api.com/images/samosa/samosa2.jpg",
+    selected: false,
+  },
+];
+
+export default function App() {
+  const [members, setMembers] = useState(initialMembers);
+  const [showAddMembers, setShowAddMembers] = useState(false);
+
+  function handlleShowAddMembers() {
+    setShowAddMembers((show) => !show);
+  }
+
+  function handleAddMembers(member) {
+    setMembers((members) => [...members, member]);
+    setShowAddMembers(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header className="header">Dinner Party</header>
+      <div className="app">
+        <div className="sidebar">
+          <InviteList members={initialMembers} />
+          <Button onClick={handlleShowAddMembers}>Add Member</Button>
+        </div>
+        <div>{showAddMembers && <FormAddMember />}</div>
+      </div>
+      <div className="menu-container">
+        <Menu item={foodItems} />
+      </div>
+    </>
+  );
+}
+function InviteList({ members }) {
+  return (
+    <>
+      <h2>Invite List</h2>
+      <ul>
+        {members.map((member) => (
+          <Member member={member} key={member.id} />
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App;
+function Member({ member }) {
+  return (
+    <li>
+      <img src={member.image} alt={member.name} />
+      <h3>{member.name}</h3>
+      <p>{member.bring ? `Bringing: ${member.bring}` : "Undecided"}</p>
+    </li>
+  );
+}
+function Menu({ item }) {
+  return (
+    <>
+      <h2>Menu</h2>
+      <ul className="menu">
+        {item.map((item) => (
+          <li>
+            <img src={item.image} alt={item.name} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+function FormAddMember() {
+  return (
+    <>
+      <form>
+        <label>🧑‍🤝‍👩Name</label>
+        <input type="text" />
+        <label>🖼️ Image URL</label>
+        <input type="text" />
+        <label>😊Select what to bring</label>
+        <select>
+          <option>1</option>
+        </select>
+        <Button>Random</Button>
+      </form>
+
+      <Button>Add</Button>
+    </>
+  );
+}
